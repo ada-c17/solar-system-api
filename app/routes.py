ae6c657089d2832ae7e8a99c81eb75da1d89d189
Jamie H <jamiewhorvath@gmail.com>
2022-04-22 14:40:39 -0400

    Created a GET response and tested successfully

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 
 class Planet:
@@ -6,22 +6,32 @@ class Planet:
         self.id = id
         self.name = name
         self.description = description
-        self.order = order
+        self.moons = moons
 
-    planets = [
-                Planet(0, "Mercury", "the nearest planet to the Sun", 0)
-                Planet(1, "Venus", "named after the Roman goddess of love and beauty", 0)
-                Planet(2, "Earth", "Home. the only astronomical object known to harbor life", 1)
-                Planet(3, "Mars", "is often called the Red Planet", 2)
-                Planet(4, "Jupiter", "more then 2.5 time the mass of all other planets", 79)
-                Planet(5, "Saturn", "second-largest planet in the Solar System", 82)
-                Planet(6, "Uranus", "named after the Greek god of the sky", 27)
-                Planet(7, "Neptune", "the densest giant planet", 14)
-                Planet(8, "Pluto", "may or may not be a planet, poor Pluto", 1)
-                ]
-    planet_bp = Blueprint("planets", __name__, url_prefix="/planets")
+planets = [
+            Planet(0, "Mercury", "the nearest planet to the Sun", 0),
+            Planet(1, "Venus", "named after the Roman goddess of love and beauty", 0),
+            Planet(2, "Earth", "Home. the only astronomical object known to harbor life", 1),
+            Planet(3, "Mars", "is often called the Red Planet", 2),
+            Planet(4, "Jupiter", "more then 2.5 time the mass of all other planets", 79),
+            Planet(5, "Saturn", "second-largest planet in the Solar System", 82),
+            Planet(6, "Uranus", "named after the Greek god of the sky", 27),
+            Planet(7, "Neptune", "the densest giant planet", 14),
+            Planet(8, "Pluto", "may or may not be a planet, poor Pluto", 1)
+            ]
+planet_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-    # @planet_bp.route("", methods=["GET"])
+@planet_bp.route("", methods=["GET"])
+def read_planets():
+    planets_response = []
+    for planet in planets:
+        planets_response.append({
+            "id": planet.id,
+            "name": planet.name,
+            "desciption": planet.description,
+            "moons": planet.moons
+        })
+    return jsonify(planets_response), 200
 
 # Define a Planet class with the attributes id, name, and description, and one additional attribute
 # Create a list of Planet instances
