37a525390c985dffdff805291a946a1bad36bc80
Danielle <dmbecker24@gmail.com>
2022-04-23 15:50:44 -0700

    Get all planets

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,31 @@
-from flask import Blueprint
 
+from flask import Blueprint, jsonify
+
+class Planet():
+    def __init__(self, id, name, description, size):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.size = size
+    
+
+planets = [
+    Planet(1, "Saturn", "Its brownish", 12),
+    Planet(2, "Earth", "Its where people live", 13),   
+    Planet(3, "Sun", "Its hot", 14)
+]
+
+planet_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+# GET ALL
+@planet_bp.route("", methods=["GET"])
+def get_all_planets():
+    planets_response = []
+    for planet in planets:
+        planets_response.append(dict(
+            id=planet.id,
+            name=planet.name,
+            description=planet.description,
+            size=planet.size
+        ))
+    return jsonify(planets_response)
