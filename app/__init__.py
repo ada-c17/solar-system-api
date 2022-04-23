6029082c4cf09a2bc8bd2d2e9644371eedd8f822
Poppy Meng <qim17@pitt.edu>
2022-04-23 15:50:47 -0700

    poppy's version wave 1

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,6 @@ from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__)
-
+    from .routes import solar_bp
+    app.register_blueprint(solar_bp)
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,33 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+
+class Planet:
+    def __init__(self, name, id, description, color):
+        self.name = name
+        self.id = id
+        self.description = description
+        self.color = color
+
+
+planets = [
+    Planet("Earth", 3, "Earth is the 3rd planet in solar system", "blue"),
+    Planet("Mars", 4, "Mars is the 4th planet in solar system", "red"),
+    Planet("Jupiter", 5, "Jupiter is the 5th planet in solar system", "tan")
+]
+
+# first string is flask blueprint name
+# run in localhost need to add/planets at the end
+solar_bp = Blueprint("", __name__, url_prefix="/planets")
+
+
+@solar_bp.route("", methods=["GET"])
+def see_planets():
+    all_planets = []
+    for planet in planets:
+        all_planets.append(
+            {"Name": planet.name,
+             "ID": planet.id,
+             "Description": planet.description,
+             "Color": planet.color}
+        )
+    return jsonify(all_planets), 200
