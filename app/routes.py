31a6638651aa245227fb45bb0105087f8161621f
Tyrah <tyrah96@gmail.com>
2022-04-25 11:59:20 -0700

    update wave 2

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planet:
     def __init__(self, id, name, description, has_moon=None):
@@ -6,6 +6,14 @@ class Planet:
         self.name = name
         self.description = description
         self.has_moon = has_moon
+
+    def to_json(self):
+        return dict(
+        id = self.id,
+        name = self.name,
+        description = self.description,
+        has_moon = self.has_moon,  
+    )
 
 planets = [
     Planet(1, "Mercury", "terrestrial", False),
@@ -22,15 +30,37 @@ with their id, name, description, and other data of the planet."""
 
 @bp.route("", methods=["GET"])
 def list_planets():
-    list_of_planets = [dict(
-        id = planet.id,
-        name = planet.name,
-        description = planet.description,
-        has_moon = planet.has_moon,  
-    ) for planet in planets]
+    list_of_planets = [planet.to_json() for planet in planets]
 
     return jsonify(list_of_planets)
 
 # FLASK_ENV=developer flask run
+
+#abort-raises and httpexception 
+#make response-overrides abort to return a flask object in html 
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        abort(make_response(jsonify(dict(details=f"invalid id : {id}")))),400
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+    abort(make_response(jsonify(dict(details=f"id not found : {id}")))),404
+
+
+@bp.route("/<planet_id>", methods=["GET"])
+def get_one_planet(planet_id):  
+    planet = validate_planet(planet_id)
+    return jsonify(planet.to_json())
+
+
+
+
+
+
+
+
+
 
     
