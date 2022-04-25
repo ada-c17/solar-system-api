b3a981ffbdcabd5e81595358a9fe6615a2f09826
Danielle <dmbecker24@gmail.com>
2022-04-25 11:51:52 -0700

    Get one planet and add errors if not found or not valid

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 
 class Planet:
@@ -7,7 +7,13 @@ class Planet:
         self.id = id
         self.description = description
         self.color = color
-
+    
+    def to_json(self):
+        return {"Name": self.name,
+                "ID": self.id,
+                "Description": self.description,
+                "Color": self.color
+                }
 
 planets = [
     Planet("Earth", 3, "Earth is the 3rd planet in solar system", "blue"),
@@ -15,6 +21,15 @@ planets = [
     Planet("Jupiter", 5, "Jupiter is the 5th planet in solar system", "tan")
 ]
 
+def validate_planet(id):
+    try:
+        id = int(id)
+    except:
+        return abort(make_response({"message": f"planet {id} is invalid"}, 400))
+    for planet in planets :
+        if planet.id == id:
+            return jsonify(planet.to_json()), 200
+    return abort(make_response({"message": f"planet {id} is not found"}, 404))
 # first string is flask blueprint name
 # run in localhost need to add/planets at the end
 solar_bp = Blueprint("", __name__, url_prefix="/planets")
@@ -25,15 +40,20 @@ def see_planets():
     all_planets = []
     for planet in planets:
         all_planets.append(
-            {"Name": planet.name,
-             "ID": planet.id,
-             "Description": planet.description,
-             "Color": planet.color}
+          planet.to_json()
         )
     return jsonify(all_planets), 200
 
+#Get one planet
+@solar_bp.route("/<id>", methods=["GET"])
+def read_one_planet(id):
+    planet_response = validate_planet(id)
+    return planet_response
 
 
 
 
-Print("xjaeo aixa8xusxfnidux ip")
+
+
+
+
