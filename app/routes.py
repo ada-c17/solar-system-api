ab83471848d38898aa23ebafda5aaf071d2b6cdb
Philomena Kelly <philomena.a.kelly@outlook.com>
2022-04-25 14:54:57 -0400

    finished wave 2, added get planet endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, jsonify
+from os import abort
+from flask import Blueprint, jsonify, make_response, abort
+
 
 class Planet:
 
@@ -7,6 +9,15 @@ class Planet:
         self.name = name
         self.description = description
         self.habitable = habitable
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "habitable": self.habitable
+        }
+        
     
 planets = [
     Planet(1, "Mercury", "Small, hot, first planet", False),
@@ -16,15 +27,30 @@ planets = [
 
 planet_bp = Blueprint("planet", __name__, url_prefix="/planets")
 
+
 @planet_bp.route("", methods=["GET"])
 def read_all_planets():
     planets_response = []
     for planet in planets:
-        planets_response.append({
-            "id": planet.id,
-            "name": planet.name,
-            "description": planet.description,
-            "habitable": planet.habitable,
-        }
+        planets_response.append(
+            planet.to_json()
         )
     return jsonify(planets_response)
+
+
+def validate_planet(id):
+    try:
+        id = int(id)
+    except:
+        return abort(make_response({"message" : f"planet {id} is invalid."}, 400))
+    
+    for planet in planets:
+        if planet.id == id:
+            return planet
+    return abort(make_response({"message" : f"planet {id} not found"}, 404))
+
+@planet_bp.route("/<id>", methods=["GET"])
+def read_one_planet(id):
+    planet = validate_planet(id)
+    return jsonify(planet.to_json())
+
