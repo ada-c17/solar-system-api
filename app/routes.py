47d6969f4bd58275619c2516710145aab4c82e19
San <san.robinxin@gmail.com>
2022-04-26 01:55:05 +0700

    update wave 2

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planet:
     def __init__(self, id, name, description, has_moon=None):
@@ -6,6 +6,15 @@ class Planet:
         self.name = name
         self.description = description
         self.has_moon = has_moon
+
+    def to_json(self):
+        return dict(
+        id = self.id,
+        name = self.name, 
+        description = self.description,
+        has_moon = self.has_moon
+            )
+
 
 planets = [
     Planet(1, "Mercury", "terrestrial", False),
@@ -20,16 +29,40 @@ bp = Blueprint("planets_bp",__name__, url_prefix="/planets")
 """..to get all existing planets, so that I can see a list of planets,
 with their id, name, description, and other data of the planet."""
 
-@bp.route("", methods=["GET"])
-def list_planets():
-    list_of_planets = [dict(
-        id = planet.id,
-        name = planet.name,
-        description = planet.description,
-        has_moon = planet.has_moon,  
-    ) for planet in planets]
+# @bp.route("", methods=["GET"])
+# def list_planets():
+#     list_of_planets = [dict(
+#         id = planet.id,
+#         name = planet.name,
+#         description = planet.description,
+#         has_moon = planet.has_moon,  
+#     ) for planet in planets]
 
-    return jsonify(list_of_planets)
+#     return jsonify(list_of_planets)
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+
+        abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+
+@bp.route("", methods=["GET"])
+def handle_planets():
+    planet_response = [planet.to_json() for planet in planets]
+
+    return jsonify(planet_response)
+
+@bp.route("/<planet_id>", methods=["GET"])
+def handle_planet(planet_id):
+    planet = validate_planet(planet_id)
+    return jsonify(planet.to_json())
+
 
 # FLASK_ENV=developer flask run
 
