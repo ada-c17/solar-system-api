edb6fe59f7927abf08c17e10fea104a5c3388d06
Ria Pastor <ria.jacqueline.pastor@gmail.com>
2022-04-25 15:53:08 -0400

    Wave 2 - get one planet implemented
    Co-authored-by: GitHub4Gigi <GitHub4Gigi@users.noreply.github.com>

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,20 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planet:
     
-    def __init__(self, name, id, description, life):
+    def __init__(self, name, id, description, has_life=None):
         self.name = name
         self.id = id
         self.description = description
-        self.life = life
+        self.has_life = has_life or False
+    
+    def to_json(self):
+        return {
+            "name": self.name,
+            "id": self.id,
+            "description": self.description,
+            "has_life": self.has_life
+        }
 
 planets = [
     Planet("Mercury", 1, "small, grey-silver", False),
@@ -16,17 +24,26 @@ planets = [
 
 planet_bp = Blueprint("planets", __name__, url_prefix= "/planets")
 
-@planet_bp.route("", methods=["GET"])
+def validate_id(id):
+    try:
+        id = int(id)
+    except:
+        abort(make_response({"message": f"{id} is not a valid planet id."},400))
+    for planet in planets:
+        if planet.id == id:
+            return planet
+    abort(make_response({"message": f"Planet with id of {id} was not found"},404))
 
+
+@planet_bp.route("", methods=["GET"])
 def get_all_planets():
     all_planets = []
     for planet in planets:
-        all_planets.append(
-            {
-                "name" : planet.name,
-                "id" : planet.id,
-                "description" : planet.description,
-                "life" : planet.life
-            }
-        )
+        all_planets.append(planet.to_json())
     return jsonify(all_planets)
+
+@planet_bp.route("/<planet_id>", methods=["GET"])
+def get_one_planet(planet_id):
+    one_planet = validate_id(planet_id)
+
+    return one_planet.to_json()
