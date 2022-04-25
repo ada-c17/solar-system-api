8d2cfe9cae9d26ac91f0675e23b14a28bddba0fd
Thu Vuong <tavuongg@gmail.com>
2022-04-25 12:21:41 -0700

    read one planet and created error messages

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, make_response, abort
 
 class Planet:
     def __init__(self, id, name, description, has_moon=None):
@@ -7,21 +7,42 @@ class Planet:
         self.description = description
         self.has_moon = has_moon
 
+    def to_dict(self):
+        return dict(
+            id = self.id,
+            name = self.name,
+            description = self.description,
+            has_moon = self.has_moon,
+        )
+
 planets = [
     Planet(1, "Mercury", "terrestrial", False),
     Planet(2, "Jupiter", "gaseous", True),
     Planet(3, "Earth", "terrestrial", True)
 ]
 
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message": f"planet {planet_id} is invalid"}, 400))
+
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+    
+    abort(make_response({"message": f"planet {planet_id} was not found"}, 404))
+
 bp = Blueprint("planets_bp",__name__, url_prefix="/planets")
 
-@bp.route("", methods=["GET"])
+@bp.route("", methods=("GET",))
 def list_planets():
-    list_of_planets = [dict(
-        id = planet.id,
-        name = planet.name,
-        description = planet.description,
-        has_moon = planet.has_moon,  
-    ) for planet in planets]
+    planet_list = [planet.to_dict() for planet in planets]
 
-    return jsonify(list_of_planets)
+    return jsonify(planet_list)
+
+@bp.route("/<planet_id>", methods=("GET",))
+def get_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    return jsonify(planet.to_dict())
