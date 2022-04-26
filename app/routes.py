c4120e17d9683906f22cb5f366eeeba94ce8d4b3
Chinazo Onwukaike <chinazoonwukaike@gmail.com>
2022-04-26 15:57:41 -0500

    wave 02

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,4 @@
-import json
-from sys import prefix
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planet:
     def __init__(self, id, name, description, color):
@@ -32,10 +30,28 @@ planets =[
 planet_bp = Blueprint("planet", __name__, url_prefix="/planets")
 
 #GET all planets
-@planet_bp.route("", methods=["GET"])
+@planet_bp.route("/", methods=["GET"])
 def get_all_planets():
     planets_lists = []
     for planet in planets:
         planets_lists.append(planet.to_json())
     
     return jsonify(planets_lists)
+
+def validate_planet(id):
+    try:
+        id = int(id)
+    except:
+        return abort(make_response({"message": f"planet {id} is invalid"}, 400))
+
+    for planet in planets:
+        if planet.id == id:
+            return planet
+            
+    return abort(make_response({"message": f"planet {id} is not found"}, 404))
+
+@planet_bp.route("<id>", methods=["GET"])
+def get_one_planet(id):
+    planet = validate_planet(id)
+
+    return jsonify(planet.to_json())
