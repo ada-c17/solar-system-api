5f18e479674c2305c4228c28cbba6532686e935f
Huma Hameed <mrskq1@gmail.com>
2022-04-25 23:08:07 -0400

    completed wave2:single planet/exception handling

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
@@ -8,6 +8,14 @@ class Planet:
         self.name = name
         self.description = description
         self.moon_count = moon_count
+    
+    def to_json(self):
+        return {
+            "id": self.id, 
+            "name": self.name,
+            "description": self.description, 
+            "moon_count": self.moon_count
+        }
 
 planets = [
     Planet(1, "Mercury", "red planet", 1),
@@ -15,15 +23,28 @@ planets = [
     Planet(1, "Earth", "water planet", 1)
 ]
 
+def validate_planet(id):
+    try:
+        id = int(id)
+    except:
+        abort(make_response({"message": f"Planet {id} is not valid"}, 400))
+
+    for planet in planets:
+        if planet.id == id:
+            return planet
+    
+    return abort(make_response({"message": f"Planet {id} not found"}, 404))
+
 @planets_bp.route("", methods=["GET"])
 def get_planets():
     planet_response_body = []
     for planet in planets:
-        planet_response_body.append({
-            "id": planet.id, 
-            "name": planet.name,
-            "description": planet.description, 
-            "moon_count": planet.moon_count
-            })
+        planet_response_body.append(planet.to_json())
             
     return jsonify(planet_response_body)
+
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def read_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    return jsonify(planet.to_json())
