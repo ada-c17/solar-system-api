604991fb597fab43bbfd52df671945fdea63cd81
jeannie <jeanniezheng123@gmail.com>
2022-04-29 13:29:48 -0500

    read all planets

--- app/routes.py
+++ app/routes.py
@@ -2,13 +2,6 @@ from app import db
 from app.models.planet import Planet
 from flask import Blueprint, jsonify, abort, make_response, request
 
-
-# planets = [
-#     Planet(1, "Mars", "Next livable planet", "131.48 million mi"),
-#     Planet(2, "Mercury", "Smallest planet", "94.025 million mi"),
-#     Planet(3, "Earth", "We live here, slowly dying", "0.0 million mi")
-# ]
-
 planet_bp = Blueprint("planet", __name__, url_prefix="/planets")
 
 
@@ -28,18 +21,14 @@ def create_planet():
     return make_response(f"Planet {new_planet.name} has been created", 201)
 
 
-# @planet_bp.route("", methods=["GET"])
-# def get_planets():
-#     planets_response = []
-#     for planet in planets:
-#         planets_response.append(
-#             {
-#                 "id": planet.id,
-#                 "name": planet.name,
-#                 "description": planet.description,
-#                 "Distance from Earth": planet.distance_from_earth
-#             })
-#     return jsonify(planets_response)
+@planet_bp.route("", methods=["GET"])
+def get_planets():
+    planets = Planet.query.all()
+    planets_response = []
+    for planet in planets:
+        planets_response.append(planet.to_json())
+
+    return jsonify(planets_response, 200)
 
 # def validate_planet(planet_id):
 #     try:
