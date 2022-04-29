da8e5654b84aae1f33c80e37058d321f4ab74c59
GitHub4Gigi <g_maycock@outlook.com>
2022-04-29 14:07:31 -0400

    Added changes to init

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import sqlalchemy
 
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    app.config["SQLAlchemy_track_modifications"] = False
+    app.config["SQLAlchemy_database_uri"] = "postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development"
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     from .routes import planet_bp
     app.register_blueprint(planet_bp)
