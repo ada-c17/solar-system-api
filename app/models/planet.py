3954ff3df185b92a2ce9d89901190f279312424f
Danielle <dmbecker24@gmail.com>
2022-05-01 15:14:52 -0700

    get Poppy's changes

--- app/models/planet.py
+++ app/models/planet.py
@@ -1,19 +1,14 @@
 from app import db
 
 class Planet(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
-<<<<<<< HEAD
     description = db.Column(db.String)
     color = db.Column(db.String)
     
-    def to_json(self):
-        return {"ID": self.id,
-                "Name": self.name,
-                "Description": self.description,
-                "Color": self.color
-                }
-=======
-    color = db.Column(db.String)
-    description=db.Column(db.String)
->>>>>>> d5b0ed65df6c5d7d8c660d713c4c1da999dc4699
+    # def to_json(self):
+    #     return {"ID": self.id,
+    #             "Name": self.name,
+    #             "Description": self.description,
+    #             "Color": self.color
+    #             }
--- app/routes.py
+++ app/routes.py
@@ -1,88 +1,22 @@
-from flask import Blueprint, jsonify, request, make_response
-from app import solar_system_development
+
+from flask import Blueprint, request, make_response
+from app import db
 from app.models.planet import Planet
 
-# class Planet:
-#     def __init__(self, name, id, description, color):
-#         self.name = name
-#         self.id = id
-#         self.description = description
-#         self.color = color
 
-
-<<<<<<< HEAD
-# class Planet:
-#     def __init__(self, name, id, description, color):
-#         self.name = name
-#         self.id = id
-#         self.description = description
-#         self.color = color
-    
-#     def to_json(self):
-#         return {"Name": self.name,
-#                 "ID": self.id,
-#                 "Description": self.description,
-#                 "Color": self.color
-#                 }
-
-=======
->>>>>>> d5b0ed65df6c5d7d8c660d713c4c1da999dc4699
-# planets = [
-#     Planet("Earth", 3, "Earth is the 3rd planet in solar system", "blue"),
-#     Planet("Mars", 4, "Mars is the 4th planet in solar system", "red"),
-#     Planet("Jupiter", 5, "Jupiter is the 5th planet in solar system", "tan")
-# ]
-
-def validate_planet(id):
-    try:
-        id = int(id)
-    except:
-        return abort(make_response({"message": f"planet {id} is invalid"}, 400))
-    for planet in planets :
-        if planet.id == id:
-            return jsonify(planet.to_json()), 200
-    return abort(make_response({"message": f"planet {id} is not found"}, 404))
-# first string is flask blueprint name
-# run in localhost need to add/planets at the end
 solar_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-# @solar_bp.route("", methods=["GET"])
-# def see_planets():
-# all_planets = []
-    # for planet in planets:
-    #     all_planets.append(
-    #         {"Name": planet.name,
-    #          "ID": planet.id,
-    #          "Description": planet.description,
-    #          "Color": planet.color}
-    #     )
-    # return jsonify(all_planets), 200
+@solar_bp.route("", methods=["POST"])
+def create_planet():
+    request_body = request.get_json()
+    new_planet = Planet(id = request_body['id'],
+                        name = request_body['name'],
+                        description = request_body['description'],
+                        color = request_body['color']
+                        )
+    
+    db.session.add(new_planet)
+    db.session.commit()
 
+    return make_response(f"Planet {new_planet.name} successfully created, 201")
 
-@solar_bp.route("", methods=["POST", "GET"])
-def handle_planets():
-    if request.method== "POST":
-        request_body=request.get_json()
-        new_planet=Planet(
-            id=request_body["id"],
-            name=request_body["name"],
-            color=request_body["color"],
-            description=request_body["description"])
-        
-        solar_system_development.session.add(new_planet)
-        solar_system_development.session.commit()
-
-        return make_response(f"Planet {new_planet.name} successfully created", 201)
-
-    elif request.method== "GET":
-        planets=Planet.query.all()
-        planets_response=[]
-        for planet in planets:
-            planets_response.append(
-                    {"Name": planet.name,
-                    "ID": planet.id,
-                    "Description": planet.description,
-                    "Color": planet.color})
-        return jsonify(planets_response), 200
-
-        
