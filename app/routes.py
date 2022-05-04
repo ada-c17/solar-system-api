12425495e2439131ed04ad33c00a614b112784cf
Natalia <natalia.woodson@gmail.com>
2022-05-03 21:10:23 -0400

    Completed wave 4: read, update, delete one planet

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+import re
 from flask import Blueprint, jsonify, abort, make_response, request
 from app import db
 from app.models.planet import Planet
@@ -15,6 +16,7 @@ from app.models.planet import Planet
 #     Planet(3, "Earth", 1, "The third planet.")
 
 # ]
+
 
 planets_bp = Blueprint("planets", __name__, url_prefix = "/planets")
 
@@ -45,25 +47,53 @@ def read_all_planets():
         })
     return jsonify(planets_response)
     
-# @planets_bp.route("<planet_id>", methods=["GET"])
-# def handle_planet(planet_id):
-#     planet = validate_planet(planet_id)
+@planets_bp.route("<planet_id>", methods=["GET"])
+def handle_planet(planet_id):
+    planet = validate_planet(planet_id)
 
-#     return {
-#         "id": planet.id,
-#         "name": planet.name,
-#         "moons": planet.moons,
-#         "description": planet.description 
-#         } 
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "moons": planet.moons,
+        "description": planet.description 
+        } 
 
-# def validate_planet(planet_id):
-#     try:
-#         planet_id = int(planet_id)
-#     except: 
-#         abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+@planets_bp.route("/<planet_id>", methods=['PUT'])
+def update_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    request_body = request.get_json()
+
+    planet.name = request_body["name"]
+    planet.moons = request_body["moons"]
+    planet.description = request_body["description"]
+
+    db.session.commit()
+
+    return make_response(f"Planet #{planet.id} successfully updated")
+
+@planets_bp.route("<planet_id>", methods=['DELETE'])
+def delete_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    db.session.delete(planet)
+    db.session.commit()
+
+    return make_response(f"Book #{planet.id} successfully deleted")
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except: 
+        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+
+    planet = Planet.query.get(planet_id)
+
+    if not planet:
+        abort(make_response({"message": f"planet {planet_id} not found"}, 404))
     
-#     for planet in planets:
-#         if planet.id == planet_id:
-#             return planet
+    return planet
+    
 
-#     abort(make_response({"message": f"planet {planet_id} not found"}, 404))
+
+    
