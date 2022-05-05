4192a9b658e0dad0c30d0775d255e164b4ce2435
Tyrah <tyrah96@gmail.com>
2022-05-05 15:43:03 -0700

    reattempt to commit to solve merge conflict

--- app/routes.py
+++ app/routes.py
@@ -1,35 +1,15 @@
-<<<<<<< HEAD
+
 import re
 from flask import Blueprint, jsonify, abort, make_response, request
 from app import db
 from .models.planet import Planet
 
-# class Planet:
-#     def __init__(self, id, name, description, has_moon=None):
-#         self.id = id
-#         self.name = name
-#         self.description = description
-#         self.has_moon = has_moon
 
-#     def to_json(self):
-#         return dict(
-#         id = self.id,
-#         name = self.name, 
-#         description = self.description,
-#         has_moon = self.has_moon
-            # )
-# planets = [
-#     Planet(1, "Mercury", "terrestrial", False),
-#     Planet(2, "Jupiter", "gaseous", True),
-#     Planet(3, "Earth", "terrestrial", True)
-# ]
 
 #instantiate blueprint object
 bp = Blueprint("planets_bp",__name__, url_prefix="/planets")
 
 #design endpoint with blueprint tag
-"""..to get all existing planets, so that I can see a list of planets,
-with their id, name, description, and other data of the planet."""
 
 @bp.route("", methods=["GET"])
 def read_all_planets():
@@ -82,31 +62,15 @@ def get_planet_by_id(id):
     try:
         id = int(id)
     except ValueError:
->>>>>>> 5952d2681d2751f9f45eb2cf8657682d11f7f463
         abort(make_response(jsonify(dict(details=f"Invalid id {id}")), 400))
 
     planet = Planet.query.get(id)
 
     if planet:
-<<<<<<< HEAD
         return planet 
 
     else: 
         abort(make_response(jsonify(dict(details=f"Invalid id {id}")), 404))
-
-@bp.route("/<planet_id>", methods=["GET"])
-def handle_planet(planet_id):
-    planet = get_planet_by_id(planet_id)
-    return jsonify(planet.to_dict())
-
-@bp.route("/<planet_id>", methods=["PUT"])
-def replace_planet(planet_id):
-    planet = get_planet_by_id(planet_id)
-    request_body = request.get_json()
-=======
-        return planet
-    else:
-        abort(make_response(jsonify(dict(details=f"Invalid planet {id}")), 404))
 
     
 #create a planet
@@ -156,20 +120,16 @@ def replace_one_planet_by_id(planet_id):
     request_body = request.get_json()
     planet = get_planet_by_id(planet_id)
 
->>>>>>> 5952d2681d2751f9f45eb2cf8657682d11f7f463
+
     planet.name = request_body["name"]
     planet.description = request_body["description"]
     planet.has_moon = request_body["has_moon"]
 
     db.session.commit()
-<<<<<<< HEAD
+
 
     return make_response(f"Planet {planet.id} successfully updated"), 200
 
-
-# FLASK_ENV=developer flask run
-=======
-    return make_response(f"Planet {planet.id} successfully added"), 200
 
 #update part of one existing planet
 @bp.route("<planet_id>", methods=["PATCH"])
@@ -191,26 +151,6 @@ def update_planet_by_id(planet_id):
     return make_response(f"Planet {planet.id} successfully updated"), 200
 
 #delete record of planet
-@bp.route("/<planet_id>", methods=["DELETE"])
-def delete_planet_by_id(planet_id):
-    planet = get_planet_by_id(planet_id)
-
-    db.session.delete(planet)
-    db.session.commit()
-
-    return make_response(f"Planet {planet.id} successfully deleted"), 200
-
-
-
-
-
-
-
-
-
-
->>>>>>> 5952d2681d2751f9f45eb2cf8657682d11f7f463
-
 @bp.route("/<planet_id>", methods=["DELETE"])
 def delete_planet(planet_id):
     planet = get_planet_by_id(planet_id)
