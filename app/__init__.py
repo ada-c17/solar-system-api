acfc351582907670992a18c3fe50e7b4544ea6dc
Ria Pastor <ria.jacqueline.pastor@gmail.com>
2022-05-05 14:49:50 -0400

    Implements features of waves 3,4 and 5

--- app/__init__.py
+++ app/__init__.py
@@ -8,11 +8,13 @@ migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    app.config["SQLAlchemy_track_modifications"] = False
-    app.config["SQLAlchemy_database_uri"] = "postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development"
 
     db.init_app(app)
     migrate.init_app(app, db)
+
+    from app.models.planet import Planet
 
     from .routes import planet_bp
     app.register_blueprint(planet_bp)
--- app/routes.py
+++ app/routes.py
@@ -1,49 +1,61 @@
-from flask import Blueprint, jsonify, abort, make_response
-
-class Planet:
-    
-    def __init__(self, name, id, description, has_life=None):
-        self.name = name
-        self.id = id
-        self.description = description
-        self.has_life = has_life or False
-    
-    def to_json(self):
-        return {
-            "name": self.name,
-            "id": self.id,
-            "description": self.description,
-            "has_life": self.has_life
-        }
-
-planets = [
-    Planet("Mercury", 1, "small, grey-silver", False),
-    Planet("Venus", 2, "hot, bright", False),
-    Planet("Earth", 3, "beautiful", True)
-]
+from app import db
+from app.models.planet import Planet
+from flask import Blueprint, jsonify, abort, make_response, request
 
 planet_bp = Blueprint("planets", __name__, url_prefix= "/planets")
-
-def validate_id(id):
-    try:
-        id = int(id)
-    except:
-        abort(make_response({"message": f"{id} is not a valid planet id."},400))
-    for planet in planets:
-        if planet.id == id:
-            return planet
-    abort(make_response({"message": f"Planet with id of {id} was not found"},404))
-
 
 @planet_bp.route("", methods=["GET"])
 def get_all_planets():
     all_planets = []
+    if len(request.args) != 0:
+        planets = Planet.query.filter_by(**request.args)
+    else:
+        planets = Planet.query.all()
+
     for planet in planets:
         all_planets.append(planet.to_json())
+
     return jsonify(all_planets)
 
 @planet_bp.route("/<planet_id>", methods=["GET"])
 def get_one_planet(planet_id):
-    one_planet = validate_id(planet_id)
+    one_planet = Planet.validate_id(planet_id)
 
     return one_planet.to_json()
+
+@planet_bp.route("", methods=["POST"])
+def create_planet():
+    planet_data = request.get_json()
+    new_planet = Planet(
+        name=planet_data['name'],
+        description=planet_data['description'],
+        has_life=planet_data['has_life']
+        )
+    
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return jsonify(f"Planet {new_planet.name} successfully created"), 201
+
+@planet_bp.route("/<planet_id>", methods=["PUT"])
+def update_planet(planet_id):
+    planet = Planet.validate_id(planet_id)
+
+    new_data = request.get_json()
+
+    planet.name = new_data['name']
+    planet.description = new_data['description']
+    planet.has_life = new_data['has_life']
+
+    db.session.commit()
+
+    return jsonify(f"Planet {planet.name} successfully updated"), 200
+
+@planet_bp.route("/<planet_id>", methods=["DELETE"])
+def delete_planet(planet_id):
+    planet = Planet.validate_id(planet_id)
+
+    db.session.delete(planet)
+    db.session.commit()
+
+    return jsonify(f"Planet {planet.name} successfully deleted"), 200
