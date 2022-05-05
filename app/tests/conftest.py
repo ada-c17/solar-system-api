8c05d352acd22c53f18b5707f51352a70f43f874
Danielle <dmbecker24@gmail.com>
2022-05-05 15:48:13 -0700

    Add planet test data pytest fixture, complete all GET tests

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,7 +1,9 @@
+
 import pytest
 from app import create_app
 from app import db
 from flask.signals import request_finished
+from app.models.planet import Planet
 
 
 @pytest.fixture
@@ -23,3 +25,14 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture
+def two_saved_planets(app):
+    planet_pluto = Planet(name="Pluto",
+                        description="nobody loves me :(",
+                        color="blue")
+    planet_sun = Planet(name="Sun",
+                        description="im hot as heck son",
+                        color="fiery red")
+    db.session.add_all([planet_pluto, planet_sun])
+    db.session.commit()
---
+++ app/tests/test_routes.py
@@ -0,0 +1,54 @@
+
+def test_get_all_planets_with_no_records(client):
+    # Act
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
+
+def test_get_all_planets(client, two_saved_planets):
+    # Act
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == [
+        {
+            "id": 1,
+            "name": "Pluto",
+            "description": "nobody loves me :(",
+            "color": "blue"
+        },
+        {
+            "id": 2,
+            "name": "Sun",
+            "description": "im hot as heck son",
+            "color": "fiery red"
+        }
+    ]
+
+def test_get_one_planet(client, two_saved_planets):
+    # Act
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "name": "Pluto",
+        "description": "nobody loves me :(",
+        "color": "blue"
+    }
+
+def test_get_one_planet_with_no_records(client):
+    # Act
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
+    assert response_body == {"message": "planet 1 not found"}
