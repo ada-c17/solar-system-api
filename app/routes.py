c32d8f2b15d679ab325cfe569193eac62405699e
Theresa Davis <perugia33@gmail.com>
2022-05-04 18:01:11 -0700

    add query param

--- app/routes.py
+++ app/routes.py
@@ -25,12 +25,19 @@ from flask import Blueprint, jsonify,abort, make_response, request
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")						
 						
-@planets_bp.route("", methods=["GET", "POST"])						
-def handle_planets():		
-    if request.method == "GET":	
-        planets = Planet.query.all()			
-        planets_response = []						
-        for planet in planets:						
+@planets_bp.route("", methods=["GET"])						
+def get_all_planets():		   			
+     
+
+        name_query = request.args.get("name")
+        if name_query:
+            planets = Planet.query.filter_by(name = name_query)
+        
+        else:
+            planets = Planet.query.all()
+
+        planets_response = []	
+        for planet in planets:	            					
             planets_response.append({					
             "id": planet.id,					
             "name": planet.name,					
@@ -38,21 +45,28 @@ def handle_planets():
             "moons": planet.moons						
     })						
         return jsonify(planets_response)	
-		
-    elif request.method == "POST":							
-        request_body = request.get_json()						
-        new_planet = Planet(						
-            name = request_body["name"],					
-            description = request_body["description"],
-            moons = request_body["moons"],				
-            )					
-							
-        db.session.add(new_planet)						
-        # staging instance new-book						
-        db.session.commit()						
-        # commit instance						
-							
-        return make_response(f"Planet {new_planet.name} created", 201)
+            
+
+
+@planets_bp.route("", methods=[ "POST"])						
+def create_planet():		
+						
+    request_body = request.get_json()						
+    new_planet = Planet(						
+        name = request_body["name"],					
+        description = request_body["description"],
+        moons = request_body["moons"],				
+        )					
+                        
+    db.session.add(new_planet)						
+    # staging instance new-book						
+    db.session.commit()						
+    # commit instance						
+                            
+    return make_response(f"Planet {new_planet.name} created", 201)
+
+
+    
 #Get Planet 
 @planets_bp.route("/<planet_id>", methods=["GET"])
 def get_planet_by_id(planet_id):
