43cdbc4b06e1e2b58c30d0deb757eb4a7e4093b3
Morgan Buntin <mmbuntin@gmail.com>
2022-05-06 09:15:24 -0400

    completed testing wave

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from app import create_app
 from app import db
 from flask.signals import request_finished
+from app.models.planet import Planet
 
 
 @pytest.fixture
@@ -23,3 +24,16 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture
+def two_saved_planets(app):
+    
+    ocean_planet = Planet(name="Caladan",
+                      description="OG home of House Atreides",
+                      moon_count=4)
+    spice_planet = Planet(name="Arrakis",
+                         description="Control Arrakis, control the universe",
+                         moon_count=2)
+
+    db.session.add_all([ocean_planet, spice_planet])
+    db.session.commit()
--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -1,3 +1,6 @@
+from urllib import response
+
+
 def test_get_all_planets_with_no_records(client):
     # Act
     response = client.get("/planets")
@@ -6,3 +9,57 @@ def test_get_all_planets_with_no_records(client):
     # Assert
     assert response.status_code == 200
     assert response_body == []
+
+def test_get_all_planets_with_saved_records(client, two_saved_planets):
+    # Act
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == [{
+        "id": 1,
+        "name": "Caladan",
+        "description": "OG home of House Atreides",
+        "moon_count": 4
+    },
+    {
+        "id": 2,
+        "name": "Arrakis",
+        "description": "Control Arrakis, control the universe",
+        "moon_count": 2 
+    }]
+
+def test_get_one_planet(client, two_saved_planets):
+    # Act
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "name": "Caladan",
+        "description": "OG home of House Atreides",
+        "moon_count": 4
+    }
+
+def test_get_one_missing_planet(client):
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    assert response.status_code == 404
+    assert response_body == {"message": "Planet 1 not found"}
+
+def test_create_one_planet(client):
+    # Act
+    response = client.post("/planets", json={
+        "name": "Giedi Prime",
+        "description": "Not a great planet",
+        "moon_count": 1
+    })
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 201
+    assert response_body == "Planet Giedi Prime successfully created"
