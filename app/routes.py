9b9f127fc318d8aff1163d970e385e7876608169
Nina <ninagetsbored@gmail.com>
2022-05-12 15:11:12 -0700

    completed wave_03

--- app/routes.py
+++ app/routes.py
@@ -1,54 +1,50 @@
-from flask import Blueprint, jsonify, abort, make_response
+from flask import Blueprint, jsonify, abort, make_response, request
+from .models.planet import Planet
+from app import db
 
-class Planet:
-    def __init__(self, id, name, description, has_moon=None):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.has_moon = has_moon
+# def validate_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except:
+#         abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
 
-planets = [
-    Planet(1, "Mercury", "terrestrial", False),
-    Planet(2, "Venus", "terrestrial", False),
-    Planet(3, "Earth", "terrestrial", True),
-    Planet(4, "Mars", "terrestrial", True)
-]
+#     for planet in planets:
+#         if planet.id == planet_id:
+#             return planet
 
-def validate_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except:
-        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
-
-    for planet in planets:
-        if planet.id == planet_id:
-            return planet
-
-    abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+#     abort(make_response({"message":f"planet {planet_id} not found"}, 404))
 
 
 bp = Blueprint("planets", __name__, url_prefix="/planets")
 
+@bp.route("", methods=("POST",))
+def post_planets():
+    request_body = request.get_json()
+
+    new_planet = Planet(name=request_body["name"],
+                        description=request_body["description"],
+                        has_moon=request_body["has_moon"],)
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} successfully created", 201)
 
 @bp.route("", methods=("GET",))
 def get_planets():
-    list_of_planets = []
-    for planet in planets:
-        list_of_planets.append({
-            "id": planet.id,
-            "name": planet.name,
-            "description": planet.description,
-            "has_moon": planet.has_moon
-        })
-    return jsonify(list_of_planets), 200
+    planets = Planet.query.all()
 
-@bp.route("/<planet_id>", methods=("GET",))
-def get_individual_planet(planet_id):
-    planet = validate_planet(planet_id)
+    result_list = [planet.to_dict() for planet in planets]
 
-    return {
-        "id": planet.id,
-        "name": planet.name,
-        "description": planet.description,
-        "has_moon": planet.has_moon
-            }, 200
+    return jsonify(result_list), 200
+
+# @bp.route("/<planet_id>", methods=("GET",))
+# def get_individual_planet(planet_id):
+#     planet = validate_planet(planet_id)
+
+#     return {
+#         "id": planet.id,
+#         "name": planet.name,
+#         "description": planet.description,
+#         "has_moon": planet.has_moon
+#             }, 200
