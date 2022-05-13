ea270a8153014328f11c68d2f8d4fe807da57375
Nina <ninagetsbored@gmail.com>
2022-05-13 08:00:40 -0700

    completed wave_05

--- app/models/planet.py
+++ app/models/planet.py
@@ -12,3 +12,14 @@ class Planet(db.Model):
             name = self.name,
             description = self.description,
             has_moon = self.has_moon,)
+
+    @classmethod
+    def from_dict(cls, data_dict):
+        return cls(name=data_dict["name"],
+                        description=data_dict["description"],
+                        has_moon=data_dict["has_moon"],)
+
+    def replace_details(self, data_dict):
+        self.name = data_dict["name"]
+        self.description = data_dict["description"]
+        self.has_moon = data_dict["has_moon"]
--- app/routes.py
+++ app/routes.py
@@ -2,18 +2,34 @@ from flask import Blueprint, jsonify, abort, make_response, request
 from .models.planet import Planet
 from app import db
 
+def error_message(message, status_code):
+    abort(make_response(jsonify(dict(details=message)), status_code))
+
 def validate_planet(planet_id):
     try:
         planet_id = int(planet_id)
     except:
-        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+        error_message(f"planet {planet_id} invalid", 400)
 
     planet = Planet.query.get(planet_id)
 
     if not planet:
-        abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+        error_message(f"planet {planet_id} not found", 404)
 
     return planet
+
+def create_planet_safely(data_dict):
+    try:
+        return Planet.from_dict(data_dict)
+    except KeyError as err:
+        error_message(f"Missing key: {err}", 400)
+
+def replace_planet_safely(planet, data_dict):
+    try:
+        planet.replace_details(data_dict)
+    except KeyError as err:
+        error_message(f"Missing key: {err}", 400)
+
 
 bp = Blueprint("planets", __name__, url_prefix="/planets")
 
@@ -21,9 +37,7 @@ bp = Blueprint("planets", __name__, url_prefix="/planets")
 def post_planet():
     request_body = request.get_json()
 
-    new_planet = Planet(name=request_body["name"],
-                        description=request_body["description"],
-                        has_moon=request_body["has_moon"],)
+    new_planet = create_planet_safely(request_body)
 
     db.session.add(new_planet)
     db.session.commit()
@@ -32,7 +46,15 @@ def post_planet():
 
 @bp.route("", methods=("GET",))
 def get_planets():
-    planets = Planet.query.all()
+    description_query = request.args.get("description")
+    has_moon_query = request.args.get("has_moon")
+
+    if description_query:
+        planets = Planet.query.filter_by(description=description_query)
+    elif has_moon_query:
+        planets = Planet.query.filter_by(has_moon=has_moon_query)
+    else:
+        planets = Planet.query.all()
 
     result_list = [planet.to_dict() for planet in planets]
 
@@ -50,9 +72,7 @@ def put_planet(planet_id):
 
     request_body = request.get_json()
 
-    planet.name = request_body["name"]
-    planet.description = request_body["description"]
-    planet.has_moon = request_body["has_moon"]
+    replace_planet_safely(planet, request_body)
 
     db.session.commit()
 
